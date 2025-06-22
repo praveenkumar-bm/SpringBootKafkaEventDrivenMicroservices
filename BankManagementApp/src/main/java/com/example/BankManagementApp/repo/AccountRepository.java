58da1963efc45919d373d53d9e5ce12293e411c8
Praveen Kumar B M <praveenbm95352@gmail.com>
2025-06-22 12:49:37 +0530

    Delete BankManagementApp directory

--- BankManagementApp/src/main/java/com/example/BankManagementApp/repo/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BankManagementApp.repo;
-
-import com.example.BankManagementApp.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-
-}
